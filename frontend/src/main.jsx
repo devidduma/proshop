5084579775a15960d1e03489087e3c1849d96222
Devid Duma <devidd05@gmail.com>
2025-02-19 18:19:59 +0100

    feat: Browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,19 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} path="/" element={<HomeScreen />} />
+        </Route>
+    )
+)
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
+        <RouterProvider router={router} />
+    </StrictMode>
 )
