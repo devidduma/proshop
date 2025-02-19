7bf25e1168e7bb05c07eea82afe0cf7cca21ad18
Devid Duma <devidd05@gmail.com>
2025-02-19 09:41:52 +0100

    feat(Main): Use custom Bootstrap, custom styles and set new logo and icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
