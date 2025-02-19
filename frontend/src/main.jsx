94dab8c6c40f06497a16030281e1c1e3723611ae
Devid Duma <devidd05@gmail.com>
2025-02-19 20:39:06 +0100

    feat(screens): Page for product details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,13 @@ import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
         </Route>
     )
 )
