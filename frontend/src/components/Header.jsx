dfccfa6728644efde152d4129877e29da4afcde5
Devid Duma <devidd05@gmail.com>
2025-02-19 18:23:15 +0100

    feat: Use Link component and Outlet

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
+import {Link} from "react-router-dom";
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -7,15 +8,15 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand as={Link} to="/">
                         <img src={logo} alt="ProShop" width="40px" height="40px" style={{padding: "4px", margin: "0 2px 0 0"}} />
                         ProShop
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                            <Nav.Link href="/login"><FaUser /> Sign in</Nav.Link>
+                            <Nav.Link as={Link} to="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                            <Nav.Link as={Link} to="/login"><FaUser /> Sign in</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
