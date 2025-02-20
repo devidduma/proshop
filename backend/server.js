f26c37bc2ead34eb953830461bc8e8f81795dec3
Devid Duma <devidd05@gmail.com>
2025-02-20 17:01:00 +0100

    feat: CORS headers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// CORS HEADERS MIDDLEWARE
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send('API is running.');
 });
