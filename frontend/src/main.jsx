13397fddf85f0ab2994166f08f408f245d07fd4a
Devid Duma <devidd05@gmail.com>
2025-02-20 17:05:04 +0100

    feat(screens): Fetch product details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import App from './App.jsx'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
+import axios from "axios";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,3 +24,6 @@ createRoot(document.getElementById('root')).render(
         <RouterProvider router={router} />
     </StrictMode>
 )
+
+/* Setup Axios baseURL */
+axios.defaults.baseURL = "http://localhost:5000";
