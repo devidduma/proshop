b6c43fdea39ef1aedbbeb6906dcfb5599553d41b
Devid Duma <devidd05@gmail.com>
2025-02-22 12:39:05 +0100

    feat(ErrorMiddleware): Middleware for Error Handling

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import path from "path";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+/**
+ * Setup
+ */
 dotenv.config({path: path.resolve(import.meta.dirname, ".env")});
-
 const port = process.env.PORT || 5000;
 const app = express();
 
-/* Connect to MongoDB */
+/**
+ * Connect to MongoDB
+ */
 connectDB();
 
-// CORS HEADERS MIDDLEWARE
+/**
+ * CORS HEADERS MIDDLEWARE
+ */
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
@@ -20,12 +27,27 @@ app.use(function (req, res, next) {
     next();
 });
 
+/**
+ * GET Root
+ */
 app.get("/", (req, res) => {
     res.send('API is running.');
 });
 
+/**
+ * Product Routes
+ */
 app.use("/api/products", productRoutes);
 
+/**
+ * Error Middleware
+ */
+app.use(notFound);
+app.use(errorHandler);
+
+/**
+ * Run Server
+ */
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
