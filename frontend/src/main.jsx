01de48b1c538afbf295459d554c7205ad3e87d57
Devid Duma <devidd05@gmail.com>
2025-02-25 18:35:11 +0100

    feat(CartScreen): Develop a screen for Shopping Cart items

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import './index.css';
+import store from './store.js';
+import axios from "axios";
+import App from './App.jsx';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
-import store from './store.js';
-import axios from "axios";
+import CartScreen from "./screens/CartScreen.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
         </Route>
     )
 )
