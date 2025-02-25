ab1a4167e5ea1c5fa890a59eee0a027a9e5d96c6
Devid Duma <devidd05@gmail.com>
2025-02-25 17:23:11 +0100

    feat(components): Number of items in Cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Badge, Container, Nav, Navbar} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
 import {Link} from "react-router-dom";
 import logo from '../assets/logo.png'
+import {useSelector} from "react-redux";
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -15,7 +18,16 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link as={Link} to="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                            <Nav.Link as={Link} to="/cart">
+                                <FaShoppingCart /> Cart
+                                {
+                                    cartItems.length > 0 && (
+                                        <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                                            { cartItems.reduce((a, c) => a + c.qty, 0) }
+                                        </Badge>
+                                    )
+                                }
+                            </Nav.Link>
                             <Nav.Link as={Link} to="/login"><FaUser /> Sign in</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
