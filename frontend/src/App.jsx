0956e1329d4959a92a86d2361b05d805deb1f4d4
Devid Duma <devidd05@gmail.com>
2025-02-27 21:39:13 +0100

    feat: Use Toast Container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header.jsx";
 import {Outlet} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 import {Container} from "react-bootstrap";
 import Footer from "./components/Footer.jsx";
 
@@ -13,6 +15,7 @@ const App = () => {
                 </Container>
             </main>
             <Footer />
+            <ToastContainer />
         </>
     )
 }
