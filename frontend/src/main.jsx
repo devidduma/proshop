f13c8a9d6c3df16c1ad0dd11478a09e361b5ec12
Devid Duma <devidd05@gmail.com>
2025-03-08 22:30:38 +0100

    feat(Main): Log Admin Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,10 +21,12 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
+import AdminRoute from "./components/AdminRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import OrderListScreen from "./screens/admin/OrderListScreen.jsx";
 
 
 const router = createBrowserRouter(
@@ -42,6 +44,10 @@ const router = createBrowserRouter(
                 <Route path="/placeorder" element={<PlaceOrderScreen />} />
                 <Route path="/order/:id" element={<OrderScreen />} />
                 <Route path="/profile" element={<ProfileScreen />} />
+            </Route>
+
+            <Route path="" element={<AdminRoute />}>
+                <Route path="/admin/orderlist" element={<OrderListScreen />} />
             </Route>
         </Route>
     )
