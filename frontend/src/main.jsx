9f75ecaadee024c5b3f24741e3504baa509a83d2
Devid Duma <devidd05@gmail.com>
2025-03-08 14:29:47 +0100

    feat(screens): Screen for updating Profile data

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,8 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import OrderScreen from "./screens/OrderScreen.jsx";
+import OrderScreen from "./screens/OrderScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 
 const router = createBrowserRouter(
@@ -40,6 +41,7 @@ const router = createBrowserRouter(
                 <Route path="/payment" element={<PaymentScreen />} />
                 <Route path="/placeorder" element={<PlaceOrderScreen />} />
                 <Route path="/order/:id" element={<OrderScreen />} />
+                <Route path="/profile" element={<ProfileScreen />} />
             </Route>
         </Route>
     )
