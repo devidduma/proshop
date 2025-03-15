18f539e597a543c66238173784f448356ae267f3
Devid Duma <devidd05@gmail.com>
2025-03-15 20:29:46 +0100

    feat(slices): Get Users query

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -29,6 +29,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 method: "PUT",
                 body: data
             })
+        }),
+        getUsers: builder.query({
+            query: () => ({
+                url: USERS_URL
+            }),
+            providesTags: ["Users"],
+            keepUnusedDataFor: 5
         })
     })
 });
@@ -38,4 +45,5 @@ export const {
     useLogoutMutation,
     useRegisterMutation,
     useProfileMutation,
+    useGetUsersQuery
 } = usersApiSlice;
