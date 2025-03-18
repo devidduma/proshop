4817323edbb356c08a8972b5e070f7d8fbe38a80
Devid Duma <devidd05@gmail.com>
2025-03-18 20:11:12 +0100

    feat(ProductListScreen): Include Pagination support

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,7 +1,9 @@
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
+import Paginate from "../../components/Paginate";
 import {
     useGetProductsQuery,
     useCreateProductMutation,
@@ -12,7 +14,9 @@ import {toast} from "react-toastify";
 
 
 const ProductListScreen = () => {
-    const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber });
 
     const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -67,7 +71,7 @@ const ProductListScreen = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    {products.map((product) => (
+                    {data.products.map((product) => (
                         <tr key={product._id}>
                             <td>{product._id}</td>
                             <td>{product.name}</td>
@@ -86,6 +90,7 @@ const ProductListScreen = () => {
                     ))}
                     </tbody>
                 </Table>
+                <Paginate pages={data.pages} page={data.page} isAdmin={true} />
             </>
         ) }
     </>;
